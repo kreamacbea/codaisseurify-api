dff603bd79c5adc80df4c0ec323d707cb214598b
kreamacbea <kreamacbea@gmail.com>
2017-09-17 20:01:36 +0200

    Create js delete songs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,52 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+function submitSong() {
+  var inputField = document.getElementById("new-song");
+  var newSong = inputField.value;
+  createSong(newSong);
+  inputField.value = null;
+
+  function createSong(title) {
+    var listItem = document.createElement("li");
+    listItem.className = "song-list-item";
+    var newSong = document.createElement("label");
+    label.innerHTML = title;
+
+    listItem.appendChild(newSong);
+
+    var list = document.getElementById("song-list");
+    list.appendChild(listItem);
+
+    function nextSongId() {
+      return document.getElementsByClassName("song-list-item").length + 1;
+    }
+  }
+  console.log();
+}
+
+function toggleDelete() {
+  var checkbox = this;
+
+  if (checkbox.checked) {
+    checkbox.parentElement.className = "song-list-item selected";
+  } else {
+    checkbox.parentElement.className = "song-list-item";
+  }
+  console.log();
+}
+
+// function selectAll() {
+//   var checkbox = this;
+//   checkbox.checked
+// }
+
+function deleteSongs() {
+  var list = document.getElementById("song-list");
+  var songsToDelete = document.getElementsByClassName("song-list-item selected");
+
+  for (var i = songsToDelete.length; i > 0; i--) {
+    list.removeChild(songsToDelete[i-1]);
+  }
+  console.log();
+}
